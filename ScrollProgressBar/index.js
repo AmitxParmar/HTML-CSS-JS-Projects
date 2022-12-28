bf8de9225389d1e99c05d251873a4e80397a9a2d
AmitxParmar <amitparmar901@gmail.com>
2022-12-29 00:53:38 +0530

    done with ScrollProgressbar

---
+++ ScrollProgressBar/index.js
@@ -0,0 +1,12 @@
+const scrollProgress = document.querySelector(".scroll-progress-bar");
+
+const height =
+    document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll", () => {
+    const scrollTop =
+        document.body.scrollTop || document.documentElement.scrollTop;
+    console.log(scrollTop)
+    scrollProgress.style.width = `${(scrollTop / height) * 100
+        }%`;
+});
